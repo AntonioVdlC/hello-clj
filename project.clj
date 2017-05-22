4e90a27f96cb7e3b925e6f77ed9db91f000ed3e8
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-22 20:24:21 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hello-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My first program in Clojure!"
+  :url "https://github.com/AntonioVdlC/hello-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
